8d91e9952c33654ad5c10b2abafaec1e02c72865
sjy_nankai <sjy_nankai@163.com>
2025-04-08 22:13:51 +0800

    change bottom select tab icon and label color

--- app/src/main/java/com/jelly/ai/MainActivity.kt
+++ app/src/main/java/com/jelly/ai/MainActivity.kt
@@ -9,11 +9,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -62,6 +64,13 @@ class MainActivity : ComponentActivity() {
                                                 contentDescription = bottomNaviItem.title
                                             )
                                         },
+                                        colors = NavigationBarItemDefaults.colors(
+                                            selectedIconColor = Color.Black,
+                                            unselectedIconColor = Color.Gray,
+                                            selectedTextColor = Color.Black,
+                                            unselectedTextColor = Color.Gray,
+                                            indicatorColor = Color.Transparent
+                                        ),
                                         onClick = {
                                             navController.navigate(bottomNaviItem.route) {
                                                 popUpTo(navController.graph.findStartDestination().id) {
